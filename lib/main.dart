2a2432da5379b4f5fdcdac28a066bb916c86110e
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-18 13:38:21 -0300

    Rearranged imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:movied/screens/main_screen.dart';
+import '/screens/main_screen.dart';
 import '/screens/splash_screen.dart';
 
 void main() async {
